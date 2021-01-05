e0228941d3fa04c043a155444562c28ce77577a6
Johannes Schnatterer <johannes@schnatterer.info>
2021-01-05 23:11:28 +0100

    Backup: Try backing up remotely

--- scripts/backup/backup-app.sh
+++ scripts/backup/backup-app.sh
@@ -9,20 +9,20 @@ RSYNC_EXTRA_ARG=${RSYNC_EXTRA_ARG:-''}
 function main() {
   echo "Backing up app $packageName to $destFolder"
 
-  mkdir  -p $destFolder/data/data/$packageName
-  doRsync /data/data/$packageName $destFolder/data/data/
+  mkdir  -p "$destFolder/data/data/$packageName"
+  doRsync "/data/data/$packageName" "$destFolder/data/data/"
 
-  if [[ -f /sdcard/Android/data/$packageName ]]; then
-    mkdir  -p $destFolder/sdcard/Android/data/$packageName
-    doRsync /sdcard/Android/data/$packageName $destFolder/sdcard/Android/data/
+  if [[ -f "/sdcard/Android/data/$packageName" ]]; then
+    mkdir  -p "$destFolder/sdcard/Android/data/$packageName"
+    doRsync "/sdcard/Android/data/$packageName" "$destFolder/sdcard/Android/data/"
   fi
 
-  # For now just take the first apk. Is this enough?
-  # No!! Reinstalling base.apk alone leads to app crashing with 
+  # TODO split APKs return multiple APKs.
+  # Realize backup and restore for them
+  # How to restore? Reinstalling base.apk alone leads to app crashing with 
   # AndroidRuntime: Caused by: java.lang.NullPointerException: Attempt to invoke interface method &apos;boolean com.google.android.play.core.missingsplits.a.a()&apos; on a null object reference 
-  apkPath=$(sudo pm path $packageName | head -n1 | sed 's/package://')
-  doRsync $apkPath $destFolder/
-  # todo for sone reason tge exttacted apk is not insyallable 
+  apkPath=$(sudo pm path "$packageName" | head -n1 | sed 's/package://')
+  doRsync "$apkPath" "$destFolder/"
 
   termux-notification --id backupApps --title  "Finished backing up app" --content "$packageName to $destFolder"
 }
@@ -30,7 +30,7 @@ function main() {
 function doRsync() {
   src="$1"
   dst="$2"
-  sudo rsync --human-readable --archive ${RSYNC_EXTRA_ARG} $src $dst
+  sudo rsync --human-readable --archive --stats ${RSYNC_EXTRA_ARG} "$src" "$dst"
 }
 
 main "$@"
--- scripts/backup/restore-app.sh
+++ scripts/backup/restore-app.sh
@@ -2,31 +2,30 @@
 if [[ -n "${DEBUG}" ]]; then set -x; fi
 set -o errexit -o nounset -o pipefail
 
-RSYNC_EXTRA_ARG=${RSYNC_EXTRA_ARG:-''}
 srcFolder="$1"
 # For now just assume folder name = package name. Reading from apk would be more defensive... and effort.
 packageName=${srcFolder##*/}
+RSYNC_EXTRA_ARG=${RSYNC_EXTRA_ARG:-''}
 
 function main() {
   echo "Restoring app $packageName from $srcFolder"
 
-  echo installing APK $srcFolder/base.apk
-  # Uninstall first?
-  sudo pm install $srcFolder/base.apk
+  echo "installing APK $srcFolder/base.apk"
+  sudo pm install "$srcFolder/base.apk"
 
-  user=$(stat -c '%U' /data/data/$packageName)
-  group=$(stat -c '%G' /data/data/$packageName)
+  user=$(stat -c '%U' "/data/data/$packageName")
+  group=$(stat -c '%G' "/data/data/$packageName")
 
-  echo restoring data to /data/data/$packageName
-  doRsync $srcFolder/data/data/$packageName /data/data/
-  echo fixing owner/group $user:$group in /data/data/$packageName
-  sudo chown -R $user:$group /data/data/$packageName
+  echo "restoring data to /data/data/$packageName"
+  doRsync "$srcFolder/data/data/$packageName" /data/data/
+  echo "fixing owner/group $user:$group in /data/data/$packageName"
+  sudo chown -R "$user:$group" "/data/data/$packageName"
   
-  if [[ -f $srcFolder/sdcard/Android/data/$packageName ]]; then
-    echo restoring data to /sdcard/Android/data/$packageName
-    doRsync $srcFolder/sdcard/Android/$packageName /sdcard/Android/
-    echo fixing owner/group $user:$group in /sdcard/Android/data/$packageName
-  sudo chown -R $user:$group /sdcard/Android/data/$packageName
+  if [[ -f "$srcFolder/sdcard/Android/data/$packageName" ]]; then
+    echo "restoring data to /sdcard/Android/data/$packageName"
+    doRsync "$srcFolder/sdcard/Android/$packageName" "/sdcard/Android/"
+    echo "fixing owner/group $user:$group in /sdcard/Android/data/$packageName"
+    sudo chown -R "$user:$group" "/sdcard/Android/data/$packageName"
   fi
   
   termux-notification --id restoreApps --title  "Finished restoring  app" --content "$packageName from $srcFolder"
@@ -35,7 +34,7 @@ function main() {
 function doRsync() {
   src="$1"
   dst="$2"
-  sudo rsync --human-readable --archive ${RSYNC_EXTRA_ARG} $src $dst
+  sudo rsync --human-readable --archive --stats  ${RSYNC_EXTRA_ARG} "$src" "$dst"
 }
 
 main "$@"
