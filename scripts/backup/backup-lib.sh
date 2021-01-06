4da8f70019c6434e730b979f43f96cc103c308f4
Johannes Schnatterer <johannes@schnatterer.info>
2021-01-06 21:17:20 +0100

    Backup: First version with SSH support

--- scripts/backup/backup-lib.sh
+++ scripts/backup/backup-lib.sh
@@ -1,7 +1,48 @@
 function doRsync() {
   src="$1"
   dst="$2"
+  remoteShellArgs=''
+  
+  if [[ "${src}" == *:* ]] || [[ "${dst}" == *:* ]]; then
+    remoteShellArgs='ssh'
+    set +o nounset
+    [[ -n "$SSH_PORT" ]] && remoteShellArgs="${remoteShellArgs} -p $SSH_PORT"
+    [[ -n "$SSH_PK" ]] && remoteShellArgs="${remoteShellArgs} -i $SSH_PK"
+    [[ -n "$SSH_HOST_FILE" ]] && remoteShellArgs="${remoteShellArgs} -o UserKnownHostsFile=$SSH_HOST_FILE"
+    set -o nounset
+    
+   #remoteShellArgs=(
+    #'ssh'
+    #)
+    #rsync $([[ ${#remoteShellArgs[@]} -ne 0 ]] && echo -p "${K3S_ARGS[*]}") 
+  
+    # ssh user@host 'mkdir -p /a/b/c'
+    eval "${remoteShellArgs} $(removeDirFromSshExpression "${dst}") 'mkdir -p $(removeUserAndHostNameFromSshExpression "${dst}")'"
+  else
+    mkdir -p "${dst}"
+  fi
+  
+  sudo rsync \
+    --human-readable --archive --stats \
+    $( [[ -n "$remoteShellArgs" ]] && echo "-e '$remoteShellArgs' ") "${src}" "${dst}"
+}
 
-  mkdir -p "$dst"
-  sudo rsync --human-readable --archive --stats ${RSYNC_EXTRA_ARG} "$src" "$dst"
+# Example:
+# user@host:/a/b/c
+# to
+# user@host
+function removeDirFromSshExpression() {
+  # shellcheck disable=SC2001 
+  # "Occasionally a more complex sed substitution is required." 
+  sed 's/\(.*\):.*/\1/' <<< "$1"
+}
+
+# Example:
+# user@host:/a/b/c
+# to
+# /a/b/c
+removeUserAndHostNameFromSshExpression() {
+  # shellcheck disable=SC2001 
+  # "Occasionally a more complex sed substitution is required." 
+  sed 's/.*:\(.*\)/\1/' <<< "$1"
 }
