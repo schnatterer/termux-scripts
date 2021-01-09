2dcda1b814d2459960f1bfd3ea15ba6b8e8b92c4
Johannes Schnatterer <johannes@schnatterer.info>
2021-01-09 23:17:37 +0100

    Backup all APKs

--- scripts/backup/backup-app.sh
+++ scripts/backup/backup-app.sh
@@ -17,12 +17,10 @@ function main() {
 
   backup "/sdcard/Android/data/$packageName"
 
-  # TODO split APKs return multiple APKs.
-  # Realize backup and restore for them
-  # How to restore? Reinstalling base.apk alone leads to app crashing with
-  # AndroidRuntime: Caused by: java.lang.NullPointerException: Attempt to invoke interface method &apos;boolean com.google.android.play.core.missingsplits.a.a()&apos; on a null object reference
-  apkPath=$(sudo pm path "$packageName" | head -n1 | sed 's/package://')
-  doRsync "$apkPath" "$baseDestFolder/"
+  # Backup all APKs from path (can be multiple for split-apks!)
+  apkPath=$(dirname "$(sudo pm path "$packageName" | head -n1 | sed 's/package://')")
+  # Only sync APKs, libs, etc are extracted during install
+  doRsync "$apkPath/" "$baseDestFolder/" -m --include='*/' --include='*.apk' --exclude='*'
 
   termux-notification --id backupApps --title "Finished backing up app" --content "$packageName to $baseDestFolder"
 }
--- scripts/backup/backup-lib.sh
+++ scripts/backup/backup-lib.sh
@@ -1,25 +1,28 @@
 function doRsync() {
   src="$1"
   dst="$2"
+  additionalArgs=( "$@" )
+  # Treat all args after src and dst as addition rsync args
+  additionalArgs=${additionalArgs[@]:2}
   remoteShellArgs=''
   
   if [[ "${src}" == *:* ]] || [[ "${dst}" == *:* ]]; then
     remoteShellArgs=('ssh')
     set +o nounset
-    [[ -n "$SSH_PORT" ]] && remoteShellArgs+=("-p $SSH_PORT") 
-    [[ -n "$SSH_PK" ]] && remoteShellArgs+=("-i $SSH_PK") 
+    [[ -n "$SSH_PORT" ]] && remoteShellArgs+=("-p $SSH_PORT")
+    [[ -n "$SSH_PK" ]] && remoteShellArgs+=("-i $SSH_PK")
     [[ -n "$SSH_HOST_FILE" ]] && remoteShellArgs+=("-o UserKnownHostsFile=$SSH_HOST_FILE")
     set -o nounset
-    
+
     # ssh user@host 'mkdir -p /a/b/c'
     eval "${remoteShellArgs[*]} $(removeDirFromSshExpression "${dst}") 'mkdir -p $(removeUserAndHostNameFromSshExpression "${dst}")'"
   else
     mkdir -p "${dst}"
   fi
-  
+
   sudo rsync \
     --human-readable --archive --stats \
-    "--rsh=${remoteShellArgs[*]}" \
+    "--rsh=${remoteShellArgs[*]}"  ${additionalArgs} \
     "${src}" "${dst}"
 }
 
@@ -28,17 +31,17 @@ function doRsync() {
 # to
 # user@host
 function removeDirFromSshExpression() {
-  # shellcheck disable=SC2001 
-  # "Occasionally a more complex sed substitution is required." 
-  sed 's/\(.*\):.*/\1/' <<< "$1"
+  # shellcheck disable=SC2001
+  # "Occasionally a more complex sed substitution is required."
+  sed 's/\(.*\):.*/\1/' <<<"$1"
 }
 
 # Example:
 # user@host:/a/b/c
 # to
 # /a/b/c
-removeUserAndHostNameFromSshExpression() {
-  # shellcheck disable=SC2001 
-  # "Occasionally a more complex sed substitution is required." 
-  sed 's/.*:\(.*\)/\1/' <<< "$1"
+function removeUserAndHostNameFromSshExpression() {
+  # shellcheck disable=SC2001
+  # "Occasionally a more complex sed substitution is required."
+  sed 's/.*:\(.*\)/\1/' <<<"$1"
 }
